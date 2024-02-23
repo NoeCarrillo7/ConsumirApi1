54e9a7747f535859697f4d2bd9a9eb9354a22bbd
NoeCarrillo7 <noelizardi7@gmail.com>
2024-02-23 12:27:57 -0600

    subir

--- src/config/rutas.js
+++ src/config/rutas.js
@@ -1,2 +1,2 @@
 export const URL_API = "https://apifirebase1.onrender.com/api";
-export const URL_IMAGES = "https://apifirebase1.onrender.com/images/";
+export const URL_IMAGES = "https://apifirebase1.onrender.com/images/";   
